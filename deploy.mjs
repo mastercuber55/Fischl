3c0dd0b3e95e9ebd0cdd459e65150ec45582fa86
mastercuber55 <mastercuber55@gmail.com>
2025-04-13 18:54:42 +0530

    testing using vercel

--- deploy.mjs
+++ deploy.mjs
@@ -1,63 +1,15 @@
-import dotenv from "dotenv"
-import { SlashCommandBuilder } from "discord.js";
 
-dotenv.config()
-
-
-export async function DiscordRequest(endpoint, options) {
-// append endpoint to root API URL
-const url = 'https://discord.com/api/v10/' + endpoint;
-// Stringify payloads
-if (options.body) options.body = JSON.stringify(options.body);
-const res = await fetch(url, {
-    headers: {
-    Authorization: `Bot ${process.env.TOKEN}`,
-    'Content-Type': 'application/json; charset=UTF-8',
-    'User-Agent': 'Marina',
-    },
-    ...options,
-});
-if (!res.ok) {
-    const data = await res.json();
-    console.log(res.status);
-    throw new Error(JSON.stringify(data));
+const template = {
+    id: '1360945709553287339',
+    application_id: '1360871728770846733',
+    default_member_permissions: null,
+    type: 1,
+    name: 'ping',
+    name_localizations: null,
+    description: 'Replies with Pong!',
+    description_localizations: null,
+    dm_permission: true,
+    contexts: [ 1 ],
+    integration_types: [ 1 ],
+    nsfw: false
 }
-return res;
-}
-
-export async function InstallGuildCommands(guildId, commands) {
-    // Change the endpoint for guild-specific commands
-    const endpoint = `applications/${process.env.CLIENT_ID}/guilds/${guildId}/commands`;
-
-    try {
-        const res = await DiscordRequest(endpoint, { method: 'PUT', body: commands });
-        console.log(await res.json())
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-export async function InstallGlobalCommands(commands) {
-    // API endpoint to overwrite global commands
-    const endpoint = `applications/${process.env.CLIENT_ID}/commands`;
-
-    try {
-        const res = await DiscordRequest(endpoint, { method: 'PUT', body: commands });
-        console.log(await res.json())
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-const commands = [
-    new SlashCommandBuilder()
-	.setName('ping')
-	.setDescription('Replies with Pong!')
-    .setContexts(1)
-    .setIntegrationTypes(1)
-    .toJSON()
-];
-
-// Call the function to install the command
-// InstallGuildCommands('1359903358781227038', commands);
-InstallGlobalCommands(commands)
