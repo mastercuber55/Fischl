507d51ab1148807c4a8c68cb7118abcb79823fb3
mastercuber55 <mastercuber55@gmail.com>
2025-08-25 18:48:38 +0530

    i forgot to pass data

--- utils/handleCmds.js
+++ utils/handleCmds.js
@@ -1,6 +1,6 @@
 export async function handleCmds(data) {
     const cmd = await import(`../cmds/${data.name}.js`)
-    const res = await cmd.run()
+    const res = await cmd.run(data)
 
     const json = { type: 4, data: res }
 
