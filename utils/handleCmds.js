87bea0176920f04c971d0158ceb319a368b0cdb2
mastercuber55 <mastercuber55@gmail.com>
2025-08-25 18:46:35 +0530

    I didnt knew i had add .js for local files

--- utils/handleCmds.js
+++ utils/handleCmds.js
@@ -1,5 +1,5 @@
 export async function handleCmds(data) {
-    const cmd = await import(`../cmds/${data.name}`)
+    const cmd = await import(`../cmds/${data.name}.js`)
     const res = await cmd.run()
 
     const json = { type: 4, data: res }
