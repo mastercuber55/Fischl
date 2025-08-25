21fdf29da37174d847a09bbf6a6f3ad5dffccd96
mastercuber55 <mastercuber55@gmail.com>
2025-08-25 19:08:43 +0530

    lets see the loaded command

--- utils/handleCmds.js
+++ utils/handleCmds.js
@@ -1,5 +1,6 @@
 export async function handleCmds(data) {
     const cmd = await import(`../cmds/${data.name}.js`)
+    console.log(cmd)
     const res = await cmd.default.run(data)
 
     const json = { type: 4, data: res }
