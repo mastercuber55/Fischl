8636ad2bef06de3acd54f8147dee4abfb165c31e
mastercuber55 <mastercuber55@gmail.com>
2025-10-07 14:03:58 +0530

    genderize command

---
+++ cmds/Fun/genderize.js
@@ -0,0 +1,31 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("a")
+        .setDescription("Guess gender of someone.")
+        .addStringOption(opt => opt
+            .setName("name")
+            .setDescription("the name to guess the gender of!")
+            .setRequired(true)
+        )
+        .toJSON(),
+    ephemeral: false,
+
+    async run({ data, user, utils }) {
+
+        const name = data.options?.find(opt => opt.name === "name")?.value
+        const r = await fetch(`https://api.genderize.io?name=${name}`);
+        const result = await r.json();
+        
+        if (result.gender) {
+            return {
+                content: `**${result.name}** is **${result.gender}** with **${result.probability * 100}%** certainty.`
+            };
+        } else {
+            return {
+                content: `Uh oh. **${result.name}** is unknown to us.`
+            }
+        }
+    }
+}
--- deploy.mjs
+++ deploy.mjs
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 import 'dotenv/config';
 import fs from "fs";
 
