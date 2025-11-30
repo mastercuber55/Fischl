dea5192431acf1e90d8efce331614178fec0ecf2
Cube Nerd <mastercuber55@gmail.com>
2025-11-30 20:20:44 +0530

    latest bleeding edge release with message components not working

--- deploy.mjs
+++ deploy.mjs
@@ -10,9 +10,9 @@ const url = `https://discord.com/api/v10/applications/${CLIENT_ID}/commands`;
 const commands = []
 const manifest = {}
 
-for(const cat of fs.readdirSync("./cmds")) {
-  for(const file of fs.readdirSync(`./cmds/${cat}`)) {
-    const cmd = await import(`./cmds/${cat}/${file}`)
+for(const cat of fs.readdirSync("./interactions/commands")) {
+  for(const file of fs.readdirSync(`./interactions/commands/${cat}`)) {
+    const cmd = await import(`./interactions/commands/${cat}/${file}`)
   
     cmd.default.data.name = file.replace(/\.js$/, "")
     cmd.default.data.contexts = [0, 1, 2]
@@ -23,8 +23,8 @@ for(const cat of fs.readdirSync("./cmds")) {
   }
 }
 
-fs.writeFileSync("./utils/manifest.json", JSON.stringify(manifest, null, 2), "utf-8");
-fs.writeFileSync("./utils/commands.json", JSON.stringify(commands, null, 2), "utf-8");
+fs.writeFileSync("./cache/categories.json", JSON.stringify(manifest, null, 2), "utf-8");
+fs.writeFileSync("./cache/commands.json", JSON.stringify(commands, null, 2), "utf-8");
 // Storing the commands as well as a cache for displaying in help command
 
 const res = await fetch(url, {
