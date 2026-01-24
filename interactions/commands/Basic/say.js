4af0a4b5360751ec46a78d469906f9c18f14acf5
Cube Nerd <mastercuber55@gmail.com>
2026-01-24 21:16:58 +0530

    PATCH: echo -> say command

---
+++ interactions/commands/Basic/say.js
@@ -0,0 +1,19 @@
+import { SlashCommandBuilder } from "discord.js"
+
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("say")
+    .setDescription("Hark! The Prinzessin echoes thy decree!")
+    .addStringOption(option => option
+      .setName("decree")
+      .setDescription("Thy decree to be echoed.")
+    ),
+  ephemeral: false,
+  run: async ({ data }) => {
+
+    const msg = data.options[0].value
+
+    return { content: msg };
+  },
+};
